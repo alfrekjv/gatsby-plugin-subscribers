eb417a2dfac800fa7cf9a8cde413a93b6cde1a0d
Alfredo Juarez <alfrekjv@gmail.com>
2019-08-19 19:09:02 -0600

    suscribers plugin

---
+++ README.md
@@ -0,0 +1,20 @@
+# gatsby-plugin-suscribers
+
+Easily add Subscribers.com Verification ID to your Gatsby site.
+
+## Install
+`npm install --save gatsby-plugin-suscribers`
+
+## How to use
+
+```javascript
+// In your gatsby-config.js
+plugins: [
+  {
+    resolve: `gatsby-plugin-suscribers`,
+    options: {
+      id: 'id here',
+    },
+  },
+]
+```
---
+++ src/gatsby-ssr.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+  if (process.env.NODE_ENV === `production`) {
+    return setHeadComponents([
+      <script
+        key={`gatsby-plugin-suscribers`}
+        dangerouslySetInnerHTML={{
+          __html: `
+            <script type="text/javascript">
+            var subscribersSiteId = '${pluginOptions.id}';
+            </script>
+            <script type="text/javascript" src="https://cdn.subscribers.com/assets/subscribers.js"></script>
+            `
+        }}
+      />
+    ])
+  }
+}
